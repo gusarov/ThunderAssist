7c82d221ba8223c5accc275ef86a16d4712c2934
Dmitry Gusarov <dmitry.gusarov@gmail.com>
2017-08-30 22:10:52 +0300

    fix tabs

--- autodeploy.js
+++ autodeploy.js
@@ -5,25 +5,25 @@ var http = require('https');
 var fs = require('fs');
 
 var download = function(url, dest, cb) {
-  var file = fs.createWriteStream(dest);
-  var request = http.get(url, function(response) {
-    response.pipe(file);
-    file.on('finish', function() {
-      file.close(cb);
-    });
-  });
+	var file = fs.createWriteStream(dest);
+	var request = http.get(url, function(response) {
+		response.pipe(file);
+		file.on('finish', function() {
+			file.close(cb);
+		});
+	});
 };
 
 download("https://codeload.github.com/gusarov/ThunderAssist/zip/master", zipPath, function() {
-    console.log("Extracting...");
-    var extract = require('extract-zip');
-    extract(zipPath, {dir: extractPath}, function (err) {
-        if (err)
-            console.error(err);
-        var copydir = require('copy-dir');
-        console.log("Copying...");
-        copydir.sync(extractPath+'\\ThunderAssist-master', 'C:\\inetpub\\wwwroot');
-        console.log("Done");
-    });
+		console.log("Extracting...");
+		var extract = require('extract-zip');
+		extract(zipPath, {dir: extractPath}, function (err) {
+				if (err)
+						console.error(err);
+				var copydir = require('copy-dir');
+				console.log("Copying...");
+				copydir.sync(extractPath+'\\ThunderAssist-master', 'C:\\inetpub\\wwwroot');
+				console.log("Done");
+		});
 });
 
